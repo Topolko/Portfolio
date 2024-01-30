cd1cc34c549bf5ed12e76a534be7db106ec3ae96
Luka <topolko.luka92@gmail.com>
2024-01-30 22:52:48 +0100

    init commit

---
+++ src/constants/index.js
@@ -0,0 +1,201 @@
+import { prehnit, conscius, morh } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    mingor,
+    ENA,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Full-stack Developer",
+        company_name: "Conscius",
+        icon: conscius,
+        iconBg: "#accbe1",
+        date: "August 2022 - ",
+        points: [
+            "Developing and maintaining web applications using Angular, C#, .Net and other related technologies.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Frontend Developer",
+        company_name: "Prehnit",
+        icon: prehnit,
+        iconBg: "#accbe1",
+        date: "January 2022 - August 2022 ",
+        points: [
+            "Developing and maintaining web applications using React, typescript and other related technologies.",
+            "Designing web apps, and rapid prototypeing components using figma",
+            "Made printable 3D models using blender and point cloud"
+        ],
+    },
+    {
+        title: "Frontend Developer",
+        company_name: "Ministarstvo obrane Republike Hrvatske",
+        icon: morh,
+        iconBg: "#accbe1",
+        date: "2013 - August 2019 ",
+        points: [
+            "Commanding the team, coordinating with platoon",
+            "Training the platoon, organizing and conducting lectures on military expertise and skills",
+            "Evaluating, advising, and mentoring company members"
+        ],
+    },
+
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Topolko',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/luka-topolko-b69968190/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: mingor,
+        theme: 'btn-back-red',
+        name: 'Registar opreme pod tlakom',
+        description: 'Developed a web application that tracks and notifies users of equipment under high pressures of impending and due inspections.',
+        link: '',
+    },
+    {
+        iconUrl: mingor,
+        theme: 'btn-back-red',
+        name: 'IndustryHelpdesk',
+        description: 'Developed a web application that provides FAQ to users and connects users to government workers via live chat.',
+        link: '',
+    },    
+    {
+        iconUrl: ENA,
+        theme: 'btn-back-red',
+        name: 'Evidencija nekretnina',
+        description: 'Worked on a web application that provides the ability to determine the actual state of surfaces and purposes of real estates for the purpose of charging a municipal fee based on aerial surveying and field work.',
+        link: '',
+    },
+    {
+        iconUrl: ENA,
+        theme: 'btn-back-red',
+        name: 'Evidencija nekretnina',
+        description: 'Worked on a web application that provides the ability to determine the actual state of surfaces and purposes of real estates for the purpose of charging a municipal fee based on aerial surveying and field work.',
+        link: '',
+    },
+
+];
---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello I'm <span className='blue-gradient_text font-semibold drop-shadow'>Luka</span>
+      </h1>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Full-stack developer
+        </p>
+      </div>
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>
+          My skills
+        </h3>
+      </div>
+      <div className='mt-16 fleex flex-wrap gap-12'>
+        
+      </div>
+    </section>
+  )
+}
+
+export default About
